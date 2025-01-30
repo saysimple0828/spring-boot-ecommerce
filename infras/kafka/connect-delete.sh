f79478d6874effb6f4416223ed76ac3e6262dd87
say.simple(김현우)/dkt <say.simple@kakaocorp.com>
2025-01-31 00:05:09 +0900

    chore: 정리

--- infras/kafka/connect-delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Delete connectors"
-echo
-
-curl -X DELETE http://localhost:8083/connectors/decosk-order-source-connect
-curl -X DELETE http://localhost:8083/connectors/decosk-order-sink-connect
-
-./connect-list.sh
--- infras/kafka/connect-list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo
-echo
-echo "Connector list"
-echo
-
-curl -X GET http://localhost:8083/connectors
